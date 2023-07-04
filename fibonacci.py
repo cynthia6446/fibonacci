18d97dd0aad9bf7500714880f1263ac9a87944d0
cynthia6446 <132763421+cynthia6446@users.noreply.github.com>
2023-07-03 18:09:19 -0700

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+## define a function that computes the n=1 st fibonnaci number, given
+# the previous two fibonacci numbers 
+
+ def aliens(F, n):
+     """
+
+     Parameters
+     ----------
+     F : integer array
+         F{n} is the n in fibonacci numbers
+     n : integer
+          index into F 
+
+     Returns
+     -------
+     F{n+1}, at the (n+1)
+
+     """
